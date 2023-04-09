69207da1e6f40b50558689536ee1d7386096acd7
Yaroslav <ytv7514@gmail.com>
2023-04-09 09:51:45 +0200

    first_commit

---
+++ script.js
@@ -0,0 +1,213 @@
+// "use strict";
+
+// a = 15;
+// console.log(a); 
+
+// let number = 5; 
+// const leftBorderWidth = 1; 
+
+
+// number = 10;
+// console.log(number); 
+
+// leftBorderWidth = 10;
+// console.log(leftBorderWidth);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 15;
+// console.log(obj)
+
+
+// console.log(name);
+// var name = 'Ivan';
+// // не использовать - устарело
+
+
+// {
+//     let result = 50;
+// }
+// console.log(result)
+
+
+// const a = 5000;
+// const b = 4000;
+
+// console.log("Щирина кузова автомобиля: " + a + ',  длинна:' + b );
+
+// const width = 5000; 
+// const leng = 4000;
+
+// console.log("Щирина кузова автомобиля: " + width + ',  длинна:' + leng );
+
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+// console.log("Щирина кузова автомобиля: " + vehicleBodyWidth + ',  длинна:' + vehicleBodyLength );
+
+// [
+//     'bruru', 'ceceerer' , 'vuvuvufufufr'
+// ].map(item =>)
+
+//request
+
+// data 
+// Response 
+
+
+//snake_case
+
+// let number = 4.6;
+
+// console.log (4/0);
+// console.log ('string' * 9);
+
+// const persone = "5";
+
+// const bool = true;  //false
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+
+// let arr = ['plum.png', 'orange.jpg',6 ,'apple.bmp' ];
+
+// console.log(arr[1]);
+
+
+// const arr = [1,2,3,4,5];
+// console.log(arr[1]);
+
+// // const obj = {
+// //     a: 1,
+// //     b: 2,
+// //     c: 3 
+// // }
+
+
+// const obj = {
+//     'Anna': 500,
+//     'Alice': 800 
+// };
+// const arr = ['a','b','c'];
+
+// arr[10]='3446';
+
+// console.log(arr);
+
+
+
+
+// const arrobj={
+//     a:1,
+//     1:2,
+//     c:3,
+//     abc:{
+//         df:[{},{}],
+//         def:{
+
+//         }
+//     }
+// }
+// const b = 'b';
+// // arrobj.b='1234';
+// arrobj['b']='1234';
+
+// console.log(arrobj['b']);
+// console.log(arrobj.b);
+
+
+// alert('hello!');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// // answers[0] = prompt('Как вос зовут?','');
+// // answers[1] = prompt('Какая ваша фамилия?','');
+// // answers[2] = prompt('Сколько вам лет?','');
+
+// console.log(typeof(answers));
+
+
+// const category = 'toys';
+// // console.log('https://someurl.com/' + category + '/' + '4');
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`ПРивет, ${user}`);
+
+
+
+// console.log('arr'+"- object");
+// console.log(4+ +"5");
+
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// ++incr;
+// --decr;
+
+// console.log(incr++);
+// console.log(decr--);
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+// console.log(2*4=='8');
+
+
+console.log(2*4===8);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+
+// const isChecked = true,
+//     isClose = true;
+
+
+// console.log(isChecked && isClose);
+
+// const isChecked = true,
+//     isClose = false;
+    
+
+// console.log(isChecked || isClose);
+
+// const isChecked = true,
+//     isClose = false;
+    
+
+// console.log(isChecked || !isClose);
+
+
+
+
+
+
